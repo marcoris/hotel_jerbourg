07b094904c47b44cc379c7bbe7f3c743d3fe4ae1
Marco Ris <ris@contria.com>
2019-08-19 08:44:41 +0200

    Adjust sql query for sales

--- models/sales_model.php
+++ models/sales_model.php
@@ -22,11 +22,13 @@ class Sales_Model extends Model
                 categories.category_id,
                 YEAR(bookings.created) AS year,
                 MONTH(bookings.created) AS month,
-                categories.price
+                SUM(categories.price) AS sales
             FROM
                 bookings
                 JOIN rooms ON (rooms.room_id = bookings.room_id)
                 JOIN categories ON (categories.category_id = rooms.category_id)
+            GROUP BY
+                month, categories.category_id
             ORDER BY year DESC, month DESC, category_id DESC'
         );
     }

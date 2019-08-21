ed6a8e2628cfb46cf552fb25e3bd27851d0f2ebd
Marco Ris <marco.ris@risdesign.ch>
2019-08-21 20:50:53 +0200

    Preparation for empty fields

--- controllers/bookings.php
+++ controllers/bookings.php
@@ -49,6 +49,9 @@ class Bookings extends Controller
         // array data to return
         $data = array();
 
+        // failurecount for empty requested fields
+        $failure = 1;
+
         // set data if guest select is not set
         if (empty($_POST['guest'])) {
             $data['salutation'] = $_POST['salutation'];
@@ -88,8 +91,12 @@ class Bookings extends Controller
         $data['arrive'] = $_POST['arrive'];
         $data['depart'] = $_POST['depart'];
 
-        // send the data to the model
-        $this->model->create($data);
+        // send the data to the model if the required fields are not empty
+        if ($failure == 0) {
+            $this->model->create($data);
+        }
+
+        // Forward to current location
         header('location: ' . URL . $this->_path);
     }
 
@@ -129,6 +136,19 @@ class Bookings extends Controller
         $data['booking_status'] = $_POST['booking_status'];
 
         $this->model->editSave($data);
+        header('location: ' . URL . $this->_path);
+    }
+
+    /**
+     * The cancel function
+     *
+     * @param integer $id The affected id to cancel
+     * 
+     * @return void
+     */
+    public function cancel($id)
+    {
+        $this->model->cancel($id);
         header('location: ' . URL . $this->_path);
     }
 

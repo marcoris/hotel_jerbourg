8a1948c1b6e8154e75b53f1450ccc1a8e5233883
Marco Ris <ris@contria.com>
2019-08-22 16:55:38 +0200

    Adjust booking CRUD functions

--- controllers/bookings.php
+++ controllers/bookings.php
@@ -49,9 +49,6 @@ class Bookings extends Controller
         // array data to return
         $data = array();
 
-        // failurecount for empty requested fields
-        $failure = 1;
-
         // set data if guest select is not set
         if (empty($_POST['guest'])) {
             $data['salutation'] = $_POST['salutation'];
@@ -59,31 +56,10 @@ class Bookings extends Controller
             $data['lastname'] = $_POST['lastname'];
             $data['birthday'] = $_POST['birthday'];
             $data['identity'] = $_POST['identity'];
-            $data['guest1_id'] = '';
+            $data['guest_id'] = '';
         } else {
             // set guest id of selected guest
-            $data['guest1_id'] = $_POST['guest'];
-        }
-
-        // set data if guest 2 select is not set
-        if (empty($_POST['guest2'])
-            && !empty($_POST['salutation2'])
-            && !empty($_POST['firstname2'])
-            && !empty($_POST['lastname2'])
-            && !empty($_POST['birthday2'])
-            && !empty($_POST['identity2'])
-        ) {
-            $data['salutation2'] = $_POST['salutation2'];
-            $data['firstname2'] = $_POST['firstname2'];
-            $data['lastname2'] = $_POST['lastname2'];
-            $data['birthday2'] = $_POST['birthday2'];
-            $data['identity2'] = $_POST['identity2'];
-            $data['guest2_id'] = '';
-        } else {
-            // set guest 2 id of selected guest
-            if (!empty($_POST['guest2'])) {
-                $data['guest2_id'] = $_POST['guest2'];
-            }
+            $data['guest_id'] = $_POST['guest'];
         }
 
         // other necessary data
@@ -92,9 +68,7 @@ class Bookings extends Controller
         $data['depart'] = $_POST['depart'];
 
         // send the data to the model if the required fields are not empty
-        if ($failure == 0) {
-            $this->model->create($data);
-        }
+        $this->model->create($data);
 
         // Forward to current location
         header('location: ' . URL . $this->_path);
@@ -128,8 +102,7 @@ class Bookings extends Controller
     {
         $data = array();
         $data['booking_id'] = $id;
-        $data['guest1_id'] = $_POST['guest'];
-        $data['guest2_id'] = !empty($_POST['guest2']) ? $_POST['guest2'] : 0;
+        $data['guest_id'] = $_POST['guest'];
         $data['room_id'] = $_POST['room'];
         $data['arrive'] = $_POST['arrive'];
         $data['depart'] = $_POST['depart'];
